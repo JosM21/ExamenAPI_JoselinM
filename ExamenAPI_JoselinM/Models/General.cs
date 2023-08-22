e4b4fe6c1c867f9484812e71b1942471d840bb96
JOSELIN MARIA CAMPOS VALVERDE <208100383@castrocarazo.ac.cr>
2023-08-22 13:52:21 -0600

    Add project files.

---
+++ ExamenAPI_JoselinM/Models/General.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExamenAPI_JoselinM.Models
+{
+    public partial class General
+    {
+        public int Idconfig { get; set; }
+        public int StrikeTop { get; set; }
+    }
+}
---
+++ ExamenAPI_JoselinM/Program.cs
@@ -0,0 +1,49 @@
+using ExamenAPI_JoselinM.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExamenAPI_JoselinM
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+
+            var CnnStringBuilder = new SqlConnectionStringBuilder(builder.Configuration.GetConnectionString("CNNSTR"));
+
+            CnnStringBuilder.Password = "123";
+
+            string cnnStr = CnnStringBuilder.ConnectionString;
+
+            builder.Services.AddDbContext<AnswersDBContext>(opcions => opcions.UseSqlServer(cnnStr));
+
+
+
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+            app.UseRouting();
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
